dc7b1b5626610a38ba07498d3dfa5cf735d84760
Tulir Asokan <tulir@maunium.net>
2024-09-02 16:54:25 +0300

    Remove unnecessary semicolons

--- pkg/connector/login-cookie.go
+++ pkg/connector/login-cookie.go
@@ -65,15 +65,15 @@ var _ bridgev2.LoginProcessCookies = (*SlackTokenLogin)(nil)
 
 const ExtractSlackTokenJS = `
 new Promise(resolve => {
-	let mautrixSlackTokenCheckInterval;
-	let useSlackInBrowserClicked = false;
+	let mautrixSlackTokenCheckInterval
+	let useSlackInBrowserClicked = false
 	function mautrixFindSlackToken() {
 		// Automatically click the "Use Slack in Browser" button
 		if (/\.slack\.com$/.test(window.location.host)) {
-			const link = document?.querySelector?.(".p-ssb_redirect__body")?.querySelector?.(".c-link");
+			const link = document?.querySelector?.(".p-ssb_redirect__body")?.querySelector?.(".c-link")
 			if (link && !useSlackInBrowserClicked) {
-				location.href = link.getAttribute("href");
-				useSlackInBrowserClicked = true;
+				location.href = link.getAttribute("href")
+				useSlackInBrowserClicked = true
 			}
 		}
 		if (!localStorage.localConfig_v2?.includes("xoxc-")) {
